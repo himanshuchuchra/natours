6a52c43c402450a2883c15011a73ca9dd8e8cd6c
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2024-01-14 21:21:36 +0530

    Write Controller Action for Get Tour

--- src/controllers/api/v1/tour.controller.js
+++ src/controllers/api/v1/tour.controller.js
@@ -25,7 +25,49 @@ export const getAllTours = async (req, res) => {
   }
 };
 
-export const getTour = (req, res) => {};
+export const getTour = async (req, res) => {
+  /*
+        Algorithm:
+
+        1. Get the data from params
+        2. Validate params is not empty
+        3. Find the data from database
+        4. Validate the data is present
+        2. Send the response
+        
+    */
+
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide path in the URL",
+      });
+    }
+
+    const tour = await Tour.findById(id);
+
+    if (!tour) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide a valid path",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Tour fetched successfully",
+      data: tour,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error,
+    });
+  }
+};
 
 export const createTour = async (req, res) => {
   /*

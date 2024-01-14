bfb637ee6fe9eb65d0e3847f0fdc57ba6941cb8c
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2024-01-14 17:30:49 +0530

    Setup Morgan - HTTP Request Logger

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 // Import Section
 import express from "express";
+import "dotenv";
+import morgan from "morgan";
 
 // Configuration Section
 const app = express();
+
+// Middleware Section
+app.use(morgan("dev")); // HTTP Request Logger
+app.use(express.json()); // Add body in req Object - req.body
 
 // Export Section
 export { app };
